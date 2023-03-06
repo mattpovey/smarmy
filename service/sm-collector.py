71963bc2d9c344f2402d57ddcd23aca58c2cb5ac
Matthew Povey - DinkumThinkum <matt.povey@gmail.com>
2023-03-06 21:16:17 +0100

    Yet another try:except

--- service/sm-collector.py
+++ service/sm-collector.py
@@ -267,5 +267,10 @@ tel_count = 0
 for telegram in serial_reader.read_as_object():
     if tel_count % 1000 == 0:
         syslog.syslog("Read " + str(tel_count) + " telegrams to Influxdb since .")
-    record_readings(sme_readings, lp_buffer)
-    tel_count += 1
+    try:
+        record_readings(sme_readings, lp_buffer)
+        tel_count += 1
+    except Exception as e:
+        syslog.syslog("Error reading telegram: " + str(e))
+        print("Error reading telegram: " + str(e))
+        sys.exit()
